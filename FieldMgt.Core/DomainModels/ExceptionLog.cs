6d6ab27c1012a2d5a7dde8dd2ef76e985c6e3227
Richa-Dits <87110521+Richa-Dits@users.noreply.github.com>
2021-07-16 10:17:27 +0530

    ExceptionLog

---
+++ FieldMgt.Core/DomainModels/ExceptionLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FieldMgt.Core.DomainModels
+{
+    public class ExceptionLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ExceptionLogId { get; set; }
+        public string Browser { get; set; }
+        public string ErrorCode { get; set; }
+        public string ExceptionId { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string ExceptionBy { get; set; }
+        [ForeignKey("ID")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime? ExceptionOn { get; set; } = DateTime.Now;
+    }
+}
+
