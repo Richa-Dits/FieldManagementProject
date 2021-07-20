f0fe0cc65df04847d2b6f2fe61818e7dac4e6d99
Richa-Dits <87110521+Richa-Dits@users.noreply.github.com>
2021-07-20 17:09:28 +0530

    UT

---
+++ FieldMgtTest/AddressTest/AddressRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FieldMgt.Core.DomainModels;
+using FieldMgt.Core.Interfaces;
+using FieldMgt.Core.UOW;
+using FieldMgt.Repository.Repository;
+using FieldMgt.Repository.UOW;
+using Moq;
+using Xunit;
+
+namespace FieldMgtTest.AddressTest
+{
+    public class AddressRepositoryTest
+    {
+        private readonly IUnitofWork _mockUOW;
+        //protected readonly AddressDetailRepository addressDetailService;
+        public AddressRepositoryTest(IUnitofWork mockUOW)
+        {
+            _mockUOW = mockUOW;
+        }
+        public void DeleteAddress_ShouldDeleteStaffMember()
+        {
+            //Arrange
+           _mockUOW.AddressRepositories.DeleteAddress
+            //Act           
+
+            //Assert
+        }
+    }
+}
